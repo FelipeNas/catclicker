dc340f172ff12056c4b36c8166c86bbe0f68bb79
Felipe Valentin Nascimento <felipe.val.nascimento@gmail.com>
2019-02-27 21:05:28 -0300

    change model, view, octupus, index and images to better

--- scripts/script.js
+++ scripts/script.js
@@ -1,78 +1,67 @@
-// model
-var model = [
-  {
-    catname: "Barry",
-    caturl: "images/barry.jpg",
-    catclicks: "0",
-    catid: "barryid"
-  },
-  {
-    catname: "Farry",
-    caturl: "images/farry.jpg",
-    catclicks: "0",
-    catid: "farryid"
-  },
-  {
-    catname: "Jerry",
-    caturl: "images/jerry.jpg",
-    catclicks: "0",
-    catid: "jerryid"
-  },
-  {
-    catname: "Larry",
-    caturl: "images/larry.jpg",
-    catclicks: "0",
-    catid: "larryid"
-  },
-  {
-    catname: "Merry",
-    caturl: "images/merry.jpg",
-    catclicks: "0",
-    catid: "marryid"
-  }
-]
+// Model
+var model = {
+  cats : [
+    {
+      catname: "Barry",
+      caturl: "images/barry.jpg",
+      catclicks: "0",
+    },
+    {
+      catname: "Farry",
+      caturl: "images/farry.jpg",
+      catclicks: "0",
+    },
+    {
+      catname: "Jerry",
+      caturl: "images/jerry.jpg",
+      catclicks: "0",
+    },
+    {
+      catname: "Larry",
+      caturl: "images/larry.jpg",
+      catclicks: "0",
+    },
+    {
+      catname: "Merry",
+      caturl: "images/merry.jpg",
+      catclicks: "0",
+    }
+  ]
+}
 
-// view
+// View
 var view = {
-  // list all cats
-  list: function(cats) {
+  // List all cats
+  list: function() {
+    var cat;
+    var cats = octopus.getCats();
     for (var i = 0; i < cats.length; i++) {
-      var catname = "<li id=" + cats[i].catid + ">" + cats[i].catname + "</li>";
-      document.getElementById("catlist").innerHTML += catname;
-    }
-  },
-  // display the cat when clicked
-  displayer: function(cats) {
-    for (var i = 0; i < cats.length; i++) {
-      var cat = cats[i]
-      var catid = cat.catid;
-      var elem = document.getElementById(catid);
-      elem.addEventListener('click', (function(cat) {
+      cat = cats[i]
+      elem = document.createElement("li");
+      elem.textContent = cat.catname;
+      elem.addEventListener("click", (function(cat) {
         return function() {
-          document.getElementById("displayarea").innerHTML = "";
-          displayname = "<h3>" + cat.catname + "</h3>";
-          displayimage = "<img src=" + cat.caturl + " id=" + cat.catid + "img>";
-          displayclicker = "<p id=" + cat.catid + "clicker>" + cat.catclicks + "</p>";
-          document.getElementById("displayarea").innerHTML = displayname + displayimage + displayclicker;
-          view.clicker(cat);
-        };
-      })(cat));
+          document.getElementById("name").textContent = cat.catname
+          document.getElementById("image").src = cat.caturl
+          document.getElementById("counter").textContent = cat.catclicks
+        }
+      })(cat))
+      document.getElementById("catlist").appendChild(elem);
     }
   },
-  // counts when clicked the cat image
-  clicker: function(cat) {
-    elem = document.getElementById(cat.catid + "img")
-    elem.addEventListener('click', function() {
-      document.getElementById(cat.catid + "clicker").innerHTML++
-      cat.catclicks++
-    }, false)
+  // Display the cat when clicked
+  // Counts when clicked the cat image
+}
+
+// Octopus
+var octopus = {
+  init: function() {
+    view.list()
+  },
+  getCats: function() {
+    return model.cats
   }
 }
 
-// octupus
-var octupus = {
-  init: [view.list(model), view.displayer(model)]
-}
-
-// start
-octupus.init
+// Start
+octopus.init()
