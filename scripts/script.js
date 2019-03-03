9a1e2cf5a6ced48c50bb46e1c7a58f0a5e00f516
Felipe Valentin Nascimento <felipe.val.nascimento@gmail.com>
2019-03-03 19:46:08 -0300

    add level feature

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,20 @@ var ViewModel = function() {
   this.name = ko.observable("Barry");
   this.imgSrc = ko.observable("https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
 
+  this.level = ko.computed(function() {
+    if (this.clickCount() < 1){
+      return "Newborn";
+    } else if (this.clickCount() >= 1 && this.clickCount() < 10) {
+      return "Baby";
+    } else if (this.clickCount() >= 10 && this.clickCount() < 20) {
+      return "Teen";
+    } else if (this.clickCount() >= 20 && this.clickCount() < 60){
+      return "Adult";
+    } else {
+      return "Senior";
+    }
+  }, this)
+
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
