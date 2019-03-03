deb9f86bba114c07e473ba2c2316664bfea91eb2
Felipe Valentin Nascimento <felipe.val.nascimento@gmail.com>
2019-03-03 20:15:46 -0300

    add nickname

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,18 @@ var ViewModel = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable("Barry");
   this.imgSrc = ko.observable("https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+  this.nickName = ko.observableArray(["B","Bary","Barryn","Weird Cat"]);
 
   this.level = ko.computed(function() {
     if (this.clickCount() < 1){
       return "Newborn";
-    } else if (this.clickCount() >= 1 && this.clickCount() < 10) {
+    } else if (this.clickCount() < 3) {
       return "Baby";
-    } else if (this.clickCount() >= 10 && this.clickCount() < 20) {
+    } else if (this.clickCount() < 10) {
+      return "Child";
+    } else if (this.clickCount() < 20) {
       return "Teen";
-    } else if (this.clickCount() >= 20 && this.clickCount() < 60){
+    } else if (this.clickCount() < 60){
       return "Adult";
     } else {
       return "Senior";
@@ -20,6 +23,7 @@ var ViewModel = function() {
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
+
 }
 
 ko.applyBindings(new ViewModel());
