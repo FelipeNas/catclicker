c19844193e9d83965c3ac3a791f63c6aa88717de
Felipe Valentin Nascimento <felipe.val.nascimento@gmail.com>
2019-03-03 21:58:42 -0300

    add with binding

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,28 @@
-var ViewModel = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable("Barry");
-  this.imgSrc = ko.observable("https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
-  this.nickName = ko.observableArray(["B","Bary","Barryn","Weird Cat"]);
+var cat = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable("Barry");
+    this.imgSrc = ko.observable("https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
+    this.nickName = ko.observableArray(["B","Bary","Barryn","Weird Cat"]);
 
-  this.level = ko.computed(function() {
-    if (this.clickCount() < 1){
-      return "Newborn";
-    } else if (this.clickCount() < 3) {
-      return "Baby";
-    } else if (this.clickCount() < 10) {
-      return "Child";
-    } else if (this.clickCount() < 20) {
-      return "Teen";
-    } else if (this.clickCount() < 60){
-      return "Adult";
-    } else {
-      return "Senior";
-    }
-  }, this)
+    this.stage = ko.computed(function() {
+      if (this.clickCount() < 1){
+        return "Newborn";
+      } else if (this.clickCount() < 3) {
+        return "Baby";
+      } else if (this.clickCount() < 10) {
+        return "Child";
+      } else if (this.clickCount() < 20) {
+        return "Teen";
+      } else if (this.clickCount() < 60){
+        return "Adult";
+      } else {
+        return "Senior";
+      }
+    }, this)
+}
+
+var ViewModel = function() {
+  this.currentCat = ko.observable(new cat());
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
